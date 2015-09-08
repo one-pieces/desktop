373fb34dae88bb66454bdee140936e96abad1eb6
Xiaolong Lin <Xiaolong.Lin@blackboard.com>
2015-09-08 11:13:37 +0800

    add "beneficiaryGroup" directive

---
+++ app/scripts/angular/beneficiaryGroupDirective.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2015/9/6 0006.
+ */
+'use strict';
+
+app
+    .directive('beneficiaryGroup', function() {
+        return {
+            restrict: 'E',
+            link : function(scope, element, attrs, ctrl) {
+
+            },
+            scope : {
+                
+            },
+            templateUrl: "views/directives/beneficiaryGroup.html",
+            controller: "beneficiaryGroupCtrl"
+        };
+    });
