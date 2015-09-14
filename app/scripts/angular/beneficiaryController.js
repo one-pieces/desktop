37be7a8e88f3ad244cfec282122eeaefb3040698
CraigWill <jingwei_xu@hotmail.com>
2015-09-14 17:53:29 +0800

    Add Indentification check

--- app/scripts/angular/beneficiaryController.js
+++ app/scripts/angular/beneficiaryController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app
-    .controller('deneficiaryCtrl', function($scope) {
+    .controller('deneficiaryCtrl', function($scope, dateFilter) {
         $scope.isChecked = false;
         $scope.add = function() {
             $scope.row.beneficiary.proportion = 
@@ -19,5 +19,20 @@ app
         $scope.$on('check-all-items', function(event, data) {
             $scope.isChecked = true;
         });
+        
+        $scope.checkIdentification = function(cardNo, birthdate) {
+            var givenYear = birthdate.getFullYear();
+            var givenMonth =birthdate.getMonth()+1;
+            var givenDay = birthdate.getDate();
+            var year = cardNo.substring(6, 10);
+            var month = cardNo.substring(10, 12);
+            var day = cardNo.substring(12, 14);
+            if(Number(givenYear) !== Number(year) || Number(givenMonth)!== Number(month) || Number(givenDay)!== Number(day)) {
+                return false;
+            }else {
+                return true;
+            }
+
+        };
 
     });
