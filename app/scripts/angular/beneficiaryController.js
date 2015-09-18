63145b418af4effc845c9e5103e9ee98b07a8621
CraigWill <jingwei_xu@hotmail.com>
2015-09-18 14:03:40 +0800

    add toogle

--- app/scripts/angular/beneficiaryController.js
+++ app/scripts/angular/beneficiaryController.js
@@ -1,12 +1,14 @@
 'use strict';
-
 app
-    .controller('deneficiaryCtrl', function($scope, dateFilter) {
+    .controller('deneficiaryCtrl', function($scope) {
         $scope.isChecked = false;
         $scope.add = function() {
             $scope.row.beneficiary.proportion = 
                 (parseInt($scope.row.beneficiary.proportion) +
                     parseInt($scope.rest));
+            $scope.changeCheckedValue();
+        };
+        $scope.changeCheckedValue = function() {
             $scope.$emit('uncheck-aver', {
                 groupIndex: $scope.groupIndex
             });
@@ -23,9 +25,12 @@ app
             $scope.isChecked = true;
         });
 
-        $scope.checkIdentification = function(cardNo, birthdate) {
-            if(!birthdate) {
-                return;
+        $scope.checkId = function(cardNo, birthdate) {
+            if(!birthdate || !cardNo || $scope.row.beneficiary.idType!=='1') {
+                return true;
+            }
+            if ( (15 !== cardNo.length && 18 !== cardNo.length)) {
+                return false;
             }
             var givenYear = birthdate.getFullYear();
             var givenMonth =birthdate.getMonth()+1;
