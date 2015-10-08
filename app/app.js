2cd4958be8c3a1cf1509b5b108ca617d927c47b1
XuJingWei <jingwei_xu@hotmail.com>
2015-10-08 18:43:48 +0800

    new function add

--- app/app.js
+++ app/app.js
@@ -1,21 +1,16 @@
 'use strict';
 
-var app = angular.module('app', ['ngAnimate','ui.bootstrap','ui.router', 'beneficiaryGroupInfo', 'beneficiaryInfo', 'beneficiaries', 'apply', 'applyInfo', 'premiumInfo',
-		'insuredInfo']);
+var app = angular.module('app', ['ngAnimate','ui.bootstrap','ui.router', 'beneficiaryGroupInfo', 'beneficiaryInfo', 'beneficiaries']);
 
 app.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', function($urlRouterProvider, $stateProvider, $locationProvider) {
 	$urlRouterProvider
-		.when('/', '/siebre/beneficiaries')
+		.when('/', '/siebre/demo')
 		.otherwise('/');
 	$stateProvider
-		.state("beneficiaries", {
-				url: "/siebre/beneficiaries",
-				templateUrl: 'views/features/beneficiaries.html',
-				controller: 'beneficiariesController'
-		}).state("apply", {
-				url: "/siebre/apply",
-				templateUrl: 'views/features/apply.html',
-				controller: 'applyController'
+		.state("demo", {
+				url: "/siebre/demo",
+				templateUrl: 'views/features/demo.html',
+				controller: 'demoCtrl'
 		});
 	$locationProvider.html5Mode(true);
 }]);
